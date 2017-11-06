af9ff161142e20503b6d5541e7fe3381006b2162
Steve Jackson <steve@testdouble.com>
2017-11-05 20:06:16 -0500

    Tweak the repo to use python3 and a static IP

--- saleor/settings.py
+++ saleor/settings.py
@@ -34,7 +34,7 @@ if os.environ.get('REDIS_URL'):
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgres://saleor:saleor@localhost:5432/saleor',
+        default='postgres://saleor:saleor@127.0.0.1:5432/saleor',
         conn_max_age=600)}
 
 
